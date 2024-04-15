05fe5749eef4bf0c224658ab46c8521c60d59295
Agnieszka Pasieka <61354571+agapasieka@users.noreply.github.com>
2024-04-15 10:21:20 +0100

    Update buckets.tf

--- challenge-labs/Monitor-and-Manage-Google-Cloud-Resources/terraform/buckets.tf
+++ challenge-labs/Monitor-and-Manage-Google-Cloud-Resources/terraform/buckets.tf
@@ -1,5 +1,13 @@
-resource "google_storage_bucket" "bucket" {
-  name          = var.bucket
+resource "google_storage_bucket" "cf_bucket" {
+  name          = var.cf-bucket
+  location      = var.region
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+}
+
+resource "google_storage_bucket" "input_bucket" {
+  name          = var.input-bucket
   location      = var.region
   force_destroy = true
 
